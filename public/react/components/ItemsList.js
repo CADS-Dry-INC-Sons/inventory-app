a8585220d4c91207cdfda70aa97c8bc90d9fcf5f
drespana <drespana@google.com>
2023-11-01 15:08:37 -0500

    organized App.js divs and started styling in style.css

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Item } from './Item';
 
 export const ItemsList = ({items, fetchItem}) => {
-	return <>
+	return (<div className='Gallery'>
 		{
 			items.map((item, idx) => {
 				return <Item item={item} key={idx} fetchItem={fetchItem}/>
 			})
 		}
-	</>
+	</div>)
 } 
